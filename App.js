4d89b99411800f626a949033b791275578fa8e3f
DevJohanR <desarrollador7777@gmail.com>
2024-11-10 22:29:42 -0500

    Carrito de compras react native 70%

--- App.js
+++ App.js
@@ -1,15 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, SafeAreaView, Switch, Text, View } from 'react-native';
+import { useColorScheme } from "nativewind" 
+import ProductsList from './components/ProductsLists';
 
 export default function App() {
+  const { colorScheme, toggleColorScheme } = useColorScheme();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView className="flex-1 bg-gray-200 items-center justify-center dark:bg-black">
+      <View className="flex-row w-full gap-5">
+      <Switch value={colorScheme === "dark"} onChange={toggleColorScheme} />
+      <Text className="dark:text-white text-2xl font-bold">New Collection</Text>
+      </View>
+      <StatusBar style={colorScheme === "dark"  ? "light" : "dark"} />
+      <ProductsList/>
+    </SafeAreaView>
   );
 }
 
+/*
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -18,3 +27,4 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+*/
